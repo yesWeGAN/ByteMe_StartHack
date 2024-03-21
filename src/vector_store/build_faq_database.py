22da0805cdf98f6a57a553cb0aed3ea421d4eeeb
Franz Birkeneder <franz.birkeneder@tum.de>
2024-03-21 01:29:43 +0100

    updates QA parsing

--- src/vector_store/build_faq_database.py
+++ src/vector_store/build_faq_database.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import os
 
 from html.parser import HTMLParser
 
@@ -7,15 +8,27 @@ class MyHTMLParser(HTMLParser):
     def __init__(self, *, convert_charrefs: bool = True) -> None:
         super().__init__(convert_charrefs=convert_charrefs)
         self.tag_of_interest =  False
+        self.is_question = False
+        self.is_answer = False
+        self.prev_was_question = False
+        self.prev_was_answer = False
+        self.prev_question_answered = False
+        self.buffer_question = None
+        self.q_and_a = {}
 
     def handle_starttag(self, tag, attrs):
         if tag == "a":  # tag is the one in brackets: <a>
             for attr in attrs:  # every tag has atttributes. attrs is a list of tuples
                 if "collapse" in attr:     # iterate over the tuples and check for keywords
-                    print("this one is collapseable and thus a question!")
+                    # print("this one is collapseable and thus a question!")
                     #print(f"Found a a-tag: {tag} with attrs {attrs}")
                     self.tag_of_interest = True    # we only want to print the data if its in a 
-        pass
+                    self.is_question = True
+        if tag == "div":
+            for attr in attrs: 
+                if "col-xs-12" in attr:
+                    self.tag_of_interest = True
+                    self.is_answer = True
 
     def handle_endtag(self, tag):   # we do not care about the end tag yet
         # print("Encountered an end tag :", tag)
@@ -24,16 +37,50 @@ class MyHTMLParser(HTMLParser):
     def handle_data(self, data):    # this one prints the content of the tag
         if len(data.strip())>0 and self.tag_of_interest:   # most paragraphs contain only whitespace
             # pass
-            print("Here's the data of your tag-of-interest:", data)
+            # print(data)
+            if self.is_question:
+                
+                if self.prev_was_question:
+                    print(f"Previous question was not answered!{data}")
+                else:
+                    self.buffer_question = data
+                    self.prev_was_question = True
+                self.prev_was_answer = False
+
+            if self.is_answer:
+                
+                if self.prev_was_answer:
+                    print(f"Answer without a question: {data}")
+                else:
+                    self.q_and_a[self.buffer_question]=data
+                    self.prev_was_answer = True
+                self.prev_was_question = False
+
             self.tag_of_interest = False
+            self.is_answer = False
+            self.is_question = False
+
     
 parser = MyHTMLParser()
 
 # put the path to your FAQ files here
-html_faq_files = Path("/Users/FrankTheTank/start/ByteMe_StartHack/src/vector_store").rglob("*.html")
-for faq in html_faq_files: 
-    print(faq.as_posix())
-    with open(faq, 'r') as htmlFile:
+html_faq_files = Path("/Users/FrankTheTank/Downloads/data").rglob("**/*faq*")
+for path in html_faq_files:
+    faq = next(iter(Path(path).rglob("*.html")))
+    with open(faq, 'r', encoding='utf8') as htmlFile:
         pure_string = htmlFile.read()
-        print(pure_string)
+        # print(pure_string)
         parser.feed(pure_string)
+
+parser.q_and_a
+filtered_q_and_a = {}
+for q, a in parser.q_and_a.items():
+    if q and "?" in q:
+        print("Question: ")
+        print(q)
+        print("Answer:")
+        print(a)
+        filtered_q_and_a[q.replace("\t", "").replace("\n","")]=a.replace("\t", "").replace("\n","")
+
+with open("q_and_a.json", 'w') as jsonout:
+    json.dump(filtered_q_and_a, jsonout)
